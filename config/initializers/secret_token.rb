b0974705b72f9303e11459d2275490690aec8d42
lbmclean <lbmclean_2000@yahoo.com>
2013-01-08 20:42:10 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '09d79bce25ac5eb74ce87b082c85dad412521802dc418476d964a2b77a8f932597aa1b780e14efda615c464281ab95c10030b669035f350476f94a2098f3508a'
